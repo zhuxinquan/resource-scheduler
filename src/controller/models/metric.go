5c42c3dfd6bed58faf28451a71bfbc1407e7db84
zhuxinquan <1174272148@qq.com>
2018-05-12 15:49:22 +0800

    commit

--- src/controller/models/metric.go
+++ src/controller/models/metric.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/cihub/seelog"
 	"io/ioutil"
+	"strings"
 )
 
 type Metrics struct{}
@@ -12,6 +13,24 @@ func (this Metrics) SetCGroupMetric(subSystemMetrics []SubSystemMetric, path str
 	seelog.Info("开始写入子系统参数")
 	for _, subSystemMetric := range subSystemMetrics {
 		subSystem := subSystemMetric.SubSystem
+		if subSystem == "memory" {
+			cgroupPath := Paths{}.JoinSubSystemPath(path, subSystem, CgroupMountPath)
+			metricPath := fmt.Sprintf("%s/%s", cgroupPath, "memory.limit_in_bytes")
+			r, err := ioutil.ReadFile(metricPath)
+			metricStr := strings.Trim(string(r), "\n")
+			metricStr = strings.Trim(metricStr, " ")
+			if metricStr == "9223372036854771712" {
+				err = ioutil.WriteFile(metricPath, []byte("1000M"), 0644)
+				if err != nil {
+					return fmt.Errorf("写入cgroup文件失败：%s", err)
+				}
+			}
+			metricPath = fmt.Sprintf("%s/%s", cgroupPath, "memory.memsw.limit_in_bytes")
+			err = ioutil.WriteFile(metricPath, []byte("9223372036854771712"), 0644)
+			if err != nil {
+				return fmt.Errorf("写入cgroup文件失败：%s", err)
+			}
+		}
 		for k, v := range subSystemMetric.Metric {
 			cgroupPath := Paths{}.JoinSubSystemPath(path, subSystem, CgroupMountPath)
 			metricPath := fmt.Sprintf("%s/%s", cgroupPath, k)
